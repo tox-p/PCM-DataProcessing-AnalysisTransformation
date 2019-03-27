11b5ee252364d6f49c312eca0ef60e37d43560d4
Philip Müller <philip-mueller@mailbox.org>
2019-03-27 17:37:40 +0100

    minor change: secure dependencies analysis updated

--- org.palladiosimulator.pcm.dataprocessing.analysis.transformation.tests/models/secure-dependencies/analysis.pl
+++ org.palladiosimulator.pcm.dataprocessing.analysis.transformation.tests/models/secure-dependencies/analysis.pl
@@ -5,7 +5,7 @@ operationProperty(_, _, _) :- fail.
 :-discontiguous(not_operationProperty/3).
 not_operationProperty(_, _, _) :- fail.
 
-secureDependencies(OP, V) :- T = 'EnumCharacteristicType Critical (_5KwWYP7hEeizQdLqRSCK5g)', isOperation(OP), hasProperty(OP, T), S=[OP|_], stackValid(S), operationReturnValue(OP, R), secureDependenciesViolation(OP, T, S, R, V).
+secureDependencies(OP, S, R, V) :- T = 'EnumCharacteristicType Critical (_5KwWYP7hEeizQdLqRSCK5g)', isOperation(OP), hasProperty(OP, T), S=[OP|_], stackValid(S), operationReturnValue(OP, R), secureDependenciesViolation(OP, T, S, R, V).
 
 secureDependenciesViolation(OP, T, S, R, V) :- valueSetMember('Enumeration CriticalEnum (_wK8iwf7hEeizQdLqRSCK5g)', V), operationProperty(OP, T, V), lnot(returnValue(S, R, T, V)).
 secureDependenciesViolation(OP, T, S, R, V) :- valueSetMember('Enumeration CriticalEnum (_wK8iwf7hEeizQdLqRSCK5g)', V), lnot(operationProperty(OP, T, V)), returnValue(S, R, T, V).
